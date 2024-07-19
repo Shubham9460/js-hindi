bbdba1423ab0c4238ebda7844dfb7227a8ee6526
Shubham9460 <sp2738723@gmail.com>
2024-07-19 12:10:47 +0000

    strings

--- index.js
+++ index.js
@@ -1,22 +1,60 @@
-//stack (primitive)  , Heap memory(Non-Primitive)
+// //stack (primitive)  , Heap memory(Non-Primitive)
 
-let myYoutubeName = "shubham"
+// let myYoutubeName = "shubham"
 
 
-let anotherName = myYoutubeName
+// let anotherName = myYoutubeName
 
-anotherName = "hey i am shubham"
-console.log("myYoutubeName");
-console.log(anotherName);
+// anotherName = "hey i am shubham"
+// console.log("myYoutubeName");
+// console.log(anotherName);
 
-let userOne = {
-    gmail: "shubham@gmail.com",
-    upi:"kdfnhs@ybl"
-}
+// let userOne = {
+//     gmail: "shubham@gmail.com",
+//     upi:"kdfnhs@ybl"
+// }
 
-let usertwo = userOne
+// let usertwo = userOne
 
-usertwo.gmail = "ram@gmail.com"
+// usertwo.gmail = "ram@gmail.com"
 
-console.log(userOne);
-console.log(usertwo.gmail)
+// console.log(userOne);
+// console.log(usertwo.gmail)
+
+
+//=========================================================================================================
+
+
+// String
+
+let name = "shubham"
+let frind = "anil"
+
+// console.log(`hey my name is ${name} and my frind name is ${frind}`);
+
+const gameName = new String(`shubham-pp`)
+
+// console.log(gameName.toUpperCase())
+// console.log(gameName.length)
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf(`u`))
+
+// const newString = gameName.substring(0 , 4)
+// console.log(newString);
+
+// const anothrString = gameName.slice(3 ,7)
+// console.log(anothrString);
+
+const newStringOne = "   shubham   "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+const url = "https//shubham.com?shubham%20parmar"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('shubham'));
+
+console.log(gameName.split('-'));
+
+console.log(String);
