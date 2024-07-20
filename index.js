11c76102a20f348823a3218dc6850088b1327b5a
Shubham9460 <sp2738723@gmail.com>
2024-07-20 06:09:05 +0000

    number

--- index.js
+++ index.js
@@ -61,3 +61,57 @@ console.log(String);
 
 
 //=====================================================================================================================================
+
+
+
+//  Number
+
+
+// const score =100
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(typeof balance);
+
+// console.log(balance.toFixed(3));
+
+// const otherNumber = 123.832837
+// console.log(otherNumber.toPrecision(3));
+
+// const hundred = 100000
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+
+//==++++++++++++++++++++++++++++++++++++++++++++++MAthes+++++++++++++++++++++++++++++++++++++++++++++++=+++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));//onliy negtiv ko possitiv karta he
+
+// console.log(Math.round(4.6));//roundfiger me values dega like 5
+
+// console.log(Math.ceil(4.6))//uper vali value lega 
+// console.log(Math.floor(4.6))//niche wali value lega lowest
+
+// console.log(Math.sqrt(5))
+// console.log(Math.pow(5 ,2))
+
+// console.log(Math.min(1,3,5,7,8));
+// console.log(Math.max(1,3,5,7,8));
+
+
+console.log(Math.random());//0 to 1
+
+console.log((Math.random() * 10) + 1);//1 to 10 se uper ki value aaygi
+
+console.log((Math.random() * 10) + 1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min)) + min);//random number ke liye min to max
